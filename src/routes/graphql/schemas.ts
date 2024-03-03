dac825994bfd5483240d5b4f6cf370aa5ca59299
Vital Raicheu <rvitaly1978@gmail.com>
2024-03-04 01:24:56 +0300

    fix: code structure

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,11 +1,11 @@
-import { Type } from '@fastify/type-provider-typebox';
+import { Type } from '@fastify/type-provider-typebox'
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
     data: Type.Any(),
     errors: Type.Any(),
   }),
-);
+)
 
 export const createGqlResponseSchema = {
   body: Type.Object(
@@ -13,8 +13,6 @@ export const createGqlResponseSchema = {
       query: Type.String(),
       variables: Type.Optional(Type.Record(Type.String(), Type.Any())),
     },
-    {
-      additionalProperties: false,
-    },
+    { additionalProperties: false },
   ),
-};
+}
