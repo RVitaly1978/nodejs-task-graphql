44595d1a8d399ff8a0f5278ab6b5b7f30e3666aa
Vital Raicheu <rvitaly1978@gmail.com>
2024-03-04 01:29:29 +0300

    fix: update fastify plugin handler

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,19 +1,40 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox'
+import { graphql, validate, parse } from 'graphql'
+import depthLimit from 'graphql-depth-limit'
+
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js'
-import { graphql } from 'graphql'
+import { gqlSchema } from './schema/schema.js'
+import { DataLoaders } from './types.js'
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
+  const { prisma } = fastify
+
+  const dataLoaders = new Map<string, DataLoaders>()
+
   fastify.route({
     url: '/',
     method: 'POST',
     schema: {
       ...createGqlResponseSchema,
-      response: {
-        200: gqlResponseSchema,
-      },
+      response: { 200: gqlResponseSchema },
     },
     async handler(req) {
-      return {}
+      const { query, variables } = req.body
+
+      const validation = validate(gqlSchema, parse(query), [depthLimit(5)])
+
+      if (validation?.length) {
+        return { data: null, errors: validation }
+      }
+
+      const response = await graphql({
+        schema: gqlSchema,
+        source: query,
+        contextValue: { prisma, dataLoaders },
+        variableValues: variables,
+      })
+
+      return response
     },
   })
 }
